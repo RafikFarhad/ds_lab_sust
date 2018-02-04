4eadaed97b35a62234e2b71c34531e7dc017ac5b
talha08 <talhaqc@gmail.com>
2018-02-04 23:21:23 +0600

    sss

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer Start -->
 <center>
 <footer class="footer">
-	Copyright &copy; Data Science Group, CSE , SUST, Sylhet, Bangladesh. All rights reserved.
+	Copyright &copy; Data Science Research Group, CSE , SUST, Sylhet, Bangladesh. All rights reserved.
 </footer>
 <!-- Footer Ends -->
 </center>
